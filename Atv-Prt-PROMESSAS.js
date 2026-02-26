51668730a916395e9b6f216acca144a52ed2c513
Uriel Barbosa <urielbarbosa@outlook.com.br>
2026-02-26 10:21:15 -0300

    Atividade Promessas

---
+++ Atv-Prt-PROMESSAS.js
@@ -0,0 +1,14 @@
+let minhaPromessa = new Promise((resolve, reject) => {
+    let sucesso = false;
+
+    if(sucesso) {resolve("Sucesso");
+    } else {
+        reject("Erro!");
+    }
+});
+
+minhaPromessa
+.then(result => {console.log(result);})
+.catch(error => {console.error(error);})
+.finally(() => {console.log("Operação concluída");
+});
